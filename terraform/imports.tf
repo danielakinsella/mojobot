b8e3fca548a9cd005f80bda044a7f2c9e680c04a
Daniela Eichner <daniela@nameface.com>
2025-12-28 20:13:23 -0500

    delete imports

--- terraform/imports.tf
+++
@@ -1,7 +0,0 @@
-# Import block for existing agent runtime
-# Remove this file after successful import
-
-import {
-  to = aws_bedrockagentcore_agent_runtime.mojobot_runtime[0]
-  id = "mojobot_agent_runtime"
-}
